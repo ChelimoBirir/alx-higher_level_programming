6599e8364febcfbcaa0ffc5add7359cd2e22f7c9
ChelimoBirir <Jelimobirir@gmail.com>
2023-06-06 06:09:42 -0700

    added string formatting to second print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,8 @@ def uppercase(str):
     ''' changes a string to uppercase
     '''
     for c in str:
-        if 97 < ord(c) < 122:
+        if 97 <= ord(c) <= 122
             print(chr(ord(c) & ~32).format(), end='')
         else:
-            print(c, end='')
+            print('{}'.format(c), end='')
     print()
