608fb680a319f159115712f00c385c2ec757e86a
ChelimoBirir <Jelimobirir@gmail.com>
2023-06-06 06:12:06 -0700

    nimechoka bruh

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,5 @@ def uppercase(str):
     ''' changes a string to uppercase
     '''
     for c in str:
-        if 97 <= ord(c) <= 122
-            print(chr(ord(c) & ~32).format(), end='')
+        print(chr(ord(c) & ~32).format(), end='')
     print()
