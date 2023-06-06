19716d1645d59155f5fe6a8552327ec30706eaa5
ChelimoBirir <Jelimobirir@gmail.com>
2023-06-06 05:14:20 -0700

    pycodestyle errors fixed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 if (number >= -9) and (number <= 9):
-    print("Last digit of {} is {} and is {}".format(number, number, number), end=' ')
+    print("Last digit of {} is {} and is {}"
+          .format(number, number, number), end=' ')
 else:
     last_digit = number - (10 * int(number / 10))
     print("Last digit of {} is {}".format(number, last_digit), end=' ')
@@ -13,4 +14,3 @@ else:
         print("and is greater than 5")
     else:
         print("and is less than 6 and not 0")
-
