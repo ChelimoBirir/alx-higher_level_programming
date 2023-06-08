356daf38d0c0c3f34c6ec5006153cfdd7710204b
ChelimoBirir <Jelimobirir@gmail.com>
2023-06-08 07:15:55 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+arguments = sys.argv[1:]
+num_arguments = len(arguments)
+
+if num_arguments == 0:
+    print("0 arguments.")
+else:
+    plural = "s" if num_arguments > 1 else ""
+
+    print("{} argument{}:".format(num_arguments, plural))
+    
+    for i, arg in enumerate(arguments):
+        print("{}: {}".format(i + 1, arg))
