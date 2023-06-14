b6902c903a704c5ab3f62a851fba1d3ebc782aee
ChelimoBirir <Jelimobirir@gmail.com>
2023-06-14 04:13:13 -0700

    Advanced tasks and pycodestylings

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    # Initialize variables to keep track of the maximum score and corresponding key
+    # variables to keep track of the maximum score and corresponding key
     max_score = float('-inf')
     max_key = None
 
